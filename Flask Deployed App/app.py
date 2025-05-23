2a65bc09773620ef2e4fdfbb947c76d9bfcbb7a7
Your Name <your.email@example.com>
2025-05-23 14:34:10 +0530

    Your update message

--- Flask Deployed App/app.py
+++ Flask Deployed App/app.py
@@ -55,7 +55,7 @@ disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
 supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
 
 model = CNN.CNN(39)    
-model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model.load_state_dict(torch.load(os.path.join(os.path.dirname(__file__), "plant_disease_model_1_latest.pt")))
 model.eval()
 
 def prediction(image_path):
